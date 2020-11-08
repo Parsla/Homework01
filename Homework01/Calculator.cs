60ddb80a8ccd35a925e81feb405d99271fdaaf1e
Parsla <parsla.eberlina@gmail.com>
2020-11-08 16:22:17 +0200

    Finished Homework01

--- Homework01/Calculator.cs
+++ Homework01/Calculator.cs
@@ -6,7 +6,38 @@ namespace Homework01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ievadi skaitli x: ");
+            string userInput1 = Console.ReadLine();
+            double x = Convert.ToDouble(userInput1);
+
+            Console.WriteLine("Ievadi skaitli y: ");
+            string userInput2 = Console.ReadLine();
+            double y = Convert.ToDouble(userInput2);
+
+            Console.WriteLine("Ievadi aritmētisko operāciju +, -, / vai *: ");
+            string userInput3 = Console.ReadLine();
+            char z = Convert.ToChar(userInput3);
+
+            if (z == '+')
+            {
+                Console.WriteLine(x + " + " + y + " = " + (Utils.Add(x, y)));
+            }
+            else if (z == '-')
+            {
+                Console.WriteLine(x + " - " + y + " = " + (Utils.Subtract(x, y)));
+            }
+            else if (z == '/')
+            {
+                Console.WriteLine(x + " / " + y + " = " + (Utils.Division(x, y)));
+            }
+            else if (z == '*')
+            {
+                Console.WriteLine(x + " * " + y + " = " + (Utils.Multiply(x, y)));
+            }
+            else
+            {
+                Console.WriteLine("Ievadītā aritmētiskā operācija neatbilst prasītajai!");
+            }
         }
     }
 }
--- Homework01/Utils.cs
+++ Homework01/Utils.cs
@@ -1,10 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace Homework01
 {
     class Utils
     {
+        public static double Add(double x, double y)
+        {
+            return x + y;
+        }
+        public static double Subtract(double x, double y)
+        {
+            return x - y;
+        }
+        public static double Division(double x, double y)
+        {
+            return x / y;
+        }
+        public static double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+
     }
 }
